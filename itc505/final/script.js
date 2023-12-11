108a3770aaf2d1f1eb0d504babb73f04512dfe5d
bharath-nalluri-13 <113639532+bharath-nalluri-13@users.noreply.github.com>
2023-12-11 15:22:38 -0700

    Add files via upload

---
+++ itc505/final/script.js
@@ -0,0 +1,99 @@
+let clickCount = 0;
+
+function toggleCell(row, col) {
+  toggleSingleCell(row, col);
+  toggleSingleCell(row - 1, col);
+  toggleSingleCell(row + 1, col);
+  toggleSingleCell(row, col - 1);
+  toggleSingleCell(row, col + 1);
+
+  checkWin();
+  updateClickCount();
+}
+
+function toggleSingleCell(row, col) {
+  const cell = document.getElementById(`cell-${row}-${col}`);
+  if (cell) {
+    cell.classList.toggle("is-off");
+  }
+}
+
+function checkWin() {
+  const allCells = document.querySelectorAll(".cell");
+  const allOff = [...allCells].every((cell) =>
+    cell.classList.contains("is-off")
+  );
+
+  if (allOff) {
+    displayWinMessage();
+  }
+}
+
+function updateClickCount() {
+  if (!document.querySelector(".win-message")) {
+    clickCount++;
+    document.getElementById("clickCount").innerText = clickCount;
+  }
+}
+
+function displayWinMessage() {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.innerHTML = `
+        <div class="modal-content">
+            <p>Hurrah! You Won!</p>
+            <button id="restartButton">Restart Game</button>
+        </div>
+    `;
+
+  document.body.appendChild(modal);
+
+  document.getElementById("restartButton").addEventListener("click", () => {
+    // Refresh the page to restart the game
+    window.location.reload();
+  });
+
+  // If the user clicks outside the modal, close it without refreshing
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.remove();
+      initializeBoard();
+      clickCount = 0;
+      document.getElementById("clickCount").innerText = clickCount;
+    }
+  });
+}
+
+// Reset the game after the user clicks OK
+setTimeout(() => {
+  winMessage.remove();
+  initializeBoard();
+  clickCount = 0;
+  document.getElementById("clickCount").innerText = clickCount;
+}, 1000);
+
+function initializeBoard() {
+  const board = document.getElementById("board");
+  board.innerHTML = "";
+  clickCount = 0;
+  document.getElementById("clickCount").innerText = clickCount;
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.id = `cell-${i}-${j}`;
+      cell.addEventListener("click", () => toggleCell(i, j));
+      board.appendChild(cell);
+    }
+  }
+
+  // Initialize the game with a random but solvable configuration
+  for (let i = 0; i < 8; i++) {
+    const randomRow = Math.floor(Math.random() * 5);
+    const randomCol = Math.floor(Math.random() * 5);
+    toggleSingleCell(randomRow, randomCol);
+  }
+}
+
+window.addEventListener("load", initializeBoard);
